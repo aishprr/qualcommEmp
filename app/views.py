d9bea06e067a98dc0f4ff2bc59f9323fec021f32
Kay Toma <ktoma@trulia.com>
2015-07-18 11:29:42 -0700

    added register event

--- app/views.py
+++ app/views.py
@@ -143,7 +143,7 @@ def conference(event_id):
     events = models.Event.query.all()
     event_name = ''
     for event in events: 
-      if(event.event_id == event_id):
+      if(event.id == event_id):
         users = eval(event.user_list)
         event_name = event.event_name
         break
@@ -166,5 +166,10 @@ def conference(event_id):
                            conf_name=event_name,
                            users=users,
                            )
+
+
+@app.route("/open")
+def open():
+  return render_template('open.html')
 
     
